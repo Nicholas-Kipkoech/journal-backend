4ee744adc23087e3a67ee12db1e19c2723444915
Nicholas Kipkoech <nickey968@gmail.com>
2025-03-17 21:21:11 +0300

    update user routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,10 +4,12 @@ import {
   registerValidation,
 } from "../validations/authValidation";
 import { AuthController } from "../controllers/authController";
-import { authenticateUser } from "../middleware/authMiddleware";
+import { authenticateUser, authorizeRole } from "../middleware/authMiddleware";
 
 const router = Router();
 
 router.post("/register", registerValidation, AuthController.register);
 router.post("/login", loginValidation, AuthController.login);
 router.get("/profile", authenticateUser, AuthController.getMe);
+
+export default router;
