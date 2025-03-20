0499cfb988b155b99dd196c7639898160521d5f1
Nicholas Kipkoech <nickey968@gmail.com>
2025-03-20 09:43:18 +0300

    add util to get image from pixabay based on moodquery

---
+++ src/utils/pixabay.ts
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+
+config();
+
+export async function getPixabayImage(query: string) {
+  try {
+    const res = await fetch(
+      `https://pixabay.com/api/?q=${query}&key=${process.env.PIXABAY_API_KEY}&min_width=1280&min_height=720&image_type=illustration&category=feelings`
+    );
+    const data = await res.json();
+    return data.hits[0]?.largeImageURL || null;
+  } catch (error) {
+    console.error("Pixabay API Error:", error);
+    return null;
+  }
+}
