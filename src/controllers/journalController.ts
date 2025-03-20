ee2506353c0500875bc4bd0e70d25ede0af59d52
Nicholas Kipkoech <nickey968@gmail.com>
2025-03-20 10:07:03 +0300

    update validation result request

---
+++ src/controllers/journalController.ts
@@ -0,0 +1,26 @@
+import { Response } from "express";
+import { CustomRequest } from "../shared";
+import { validationResult } from "express-validator";
+import { JournalService } from "../services/journalService";
+
+// user request logic
+export class JournalController {
+  // add user journals
+  static async addJournal(req: CustomRequest, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
+    try {
+      const journalData = req.body;
+      const newJournal = await JournalService.addJournal(
+        req.user.id,
+        journalData
+      );
+      res
+        .status(200)
+        .json({ message: "Journal added successfully", data: newJournal });
+    } catch (error) {
+      console.error("error adding journal", error);
+      res.status(500).json({ error: error.message });
+    }
+  }
+}
