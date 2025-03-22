71791d48373abe9ea630f8f0293308873cadcca4
Nicholas Kipkoech <nickey968@gmail.com>
2025-03-22 10:21:55 +0300

    fix collection deletion params

--- src/controllers/collectionController.ts
+++ src/controllers/collectionController.ts
@@ -36,7 +36,7 @@ export class CollectionController {
   // Delete user collections based on collection id
   static async deleteCollection(req: CustomRequest, res: Response) {
     try {
-      const { collectionId } = req.body;
+      const { collectionId }: string | any = req.query;
       if (!collectionId) {
         res.status(400).json({ message: "Collection ID is required!!" });
       }
